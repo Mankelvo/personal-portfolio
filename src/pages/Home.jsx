ea233739def388574045b7c0b33b24d743d2efd3
Kelvin <kelvostudy@gmail.com>
2026-04-24 20:52:21 -0400

    Add Hero section component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar'
+import Hero from '../components/sections/Hero';
 function Home(){
     return(
    <>
     <Navbar/>
     <main>Home Page</main>
+    <Hero/>
    <Footer/>
    </>
     )
