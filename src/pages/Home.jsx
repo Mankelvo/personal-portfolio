b76e3ed05115c9d319839c1e1032a7812c4bb513
Kelvin <kelvostudy@gmail.com>
2026-04-24 20:59:42 -0400

    Add about me section component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar'
 import Hero from '../components/sections/Hero';
+import About from '../components/sections/About';
 function Home(){
     return(
    <>
     <Navbar/>
     <main>Home Page</main>
     <Hero/>
+    <About/>
    <Footer/>
    </>
     )
