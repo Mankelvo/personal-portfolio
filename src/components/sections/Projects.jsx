b7e74d902595a6467b40f255c8b02db058ffbd7f
Kelvin <kelvostudy@gmail.com>
2026-04-24 21:23:44 -0400

    Add Project section component

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,14 @@
+function Projects(){
+    return (
+        
+        <>
+        <h1>Projects</h1>
+        <p>Project 1</p>
+        <p>Project 2</p>
+        <p>Project 3</p>
+
+        </>
+    )
+}
+
+export default Projects;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../components/layout/Navbar'
 import Hero from '../components/sections/Hero';
 import About from '../components/sections/About';
 import Skills from '../components/sections/Skills';
+import Projects from '../components/sections/Projects';
+
 
 function Home(){
     return(
@@ -12,6 +14,7 @@ function Home(){
     <Hero/>
     <About/>
     <Skills/>
+    <Projects/>
    <Footer/>
    </>
     )
