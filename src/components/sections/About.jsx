994a6c34d303899d3bbcd147619943fdfbd167af
Kelvin <kelvostudy@gmail.com>
2026-04-25 10:35:39 -0400

    Update About section content

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,6 +1,20 @@
 function About(){
     return (
+        <section>
         <h2>About Me</h2>
+        <p>
+          I’m a passionate Frontend Developer focused on building clean,
+    responsive, and user-friendly web applications using React,
+    JavaScript, TypeScript, HTML, CSS, and Tailwind CSS.
+        </p>
+        <p>
+           With a strong problem-solving mindset and a background in
+    Information Technology, I enjoy turning ideas into real-world
+    digital solutions. I am currently seeking opportunities where I
+    can contribute, grow, and build impactful digital experiences. 
+        </p>
+
+        </section>
     )
 }
 
