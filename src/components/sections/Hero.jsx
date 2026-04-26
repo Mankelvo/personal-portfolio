574b23a75286bbe29408fb444ccef98279728556
Kelvin <kelvostudy@gmail.com>
2026-04-26 11:58:41 -0400

    configured tailwind and tested with hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,7 +6,7 @@ function Hero(){
         <section>
                <div>
         <p>Hi, I'm Kelvin</p>
-        <h1>React Frontend Developer</h1>
+        <h1 className="text-4x1 font-bold text-blue-600">React Frontend Developer</h1>
         <p>I build responsive, user-friendly web applications with React,
         JavaScript, and modern frontend tools.</p>
 
