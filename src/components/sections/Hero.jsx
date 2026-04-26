1a17857a5e9a08f798e077b84433318d4806f214
Kelvin <kelvostudy@gmail.com>
2026-04-26 11:33:58 -0400

    add profile image to hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,14 +1,25 @@
+import Kelvin from "../../assets/images/Kelvin.jpeg";
+
+
 function Hero(){
     return (
         <section>
+               <div>
         <p>Hi, I'm Kelvin</p>
         <h1>React Frontend Developer</h1>
         <p>I build responsive, user-friendly web applications with React,
         JavaScript, and modern frontend tools.</p>
-        <div>
-        <button>View Projects</button>
+
+     <div>
+ <button>View Projects</button>
         <button>Contact Me</button>
         <button>Download Resume</button>
+     </div>
+       
+        </div>
+        <div>
+                    <img src={Kelvin} alt="Kelvin Portrait"/>
+
         </div>
         </section>
     )
