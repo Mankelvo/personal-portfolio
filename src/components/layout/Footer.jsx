ef0e0c9c3934c55e13ae5b01237a3545e7104612
Kelvin <kelvostudy@gmail.com>
2026-04-25 21:33:27 -0400

    update footer content

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,16 @@
 function Footer(){
     return(
-        <h1>This is my footer</h1>
+        <footer>
+            <p>Kelvin</p>
+            <p>Frontend Developer</p>
+            <div>
+                <a href="https://github.com/Mankelvo" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/kelvin-maina-789086154" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="mailto:kelvostudy@gmail.com">Email</a>
+            </div>
+             <p>© 2026 Kelvin. All rights reserved</p>
+
+        </footer>
     )
 }
 
