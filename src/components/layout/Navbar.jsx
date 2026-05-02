8331fd78a62f34ca95f57faa67f1376dc3d82746
Kelvin <kelvostudy@gmail.com>
2026-05-02 09:47:37 -0400

    Style Navbar section

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,15 @@
 function Navbar(){
+    const NavLink = "hover:text-blue-600 transition "
     return(
-        <nav>
-            <a href="#">Kelvin</a>
-            <ul>
+        <nav className="flex justify-between items-center bg-white/70 backdrop-blur-md max-w-6xl mx-auto sticky top-0 z-50 ">
+            <a  className="hover:text-blue-600 transition tx-xl font-bold"href="#">Kelvin</a> 
+       <ul className="flex justify-around gap-8 items-center px-8 py-4">
             
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#">Resume</a></li>
+                <li ><a  className={NavLink} href="#about">About</a></li>
+                <li><a   className={NavLink}  href="#skills">Skills</a></li>
+                <li><a  className={NavLink}>Projects</a></li>
+                <li><a  className={NavLink} href="#contact">Contact</a></li>
+                <li><a  className={NavLink} href="#">Resume</a></li>
             </ul>
         </nav>
  
